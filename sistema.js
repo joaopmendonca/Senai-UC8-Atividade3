3ee5773c2cd3c19c037360457dde64f89d0dc21d
João Paulo <82663879+joaopmendonca@users.noreply.github.com>
2022-01-28 16:15:29 -0300

    Add files via upload

---
+++ sistema.js
@@ -0,0 +1,60 @@
+let nomeDeAluno = ["Marcel", "Joao", "Afranio"]
+for (let nome of nomeDeAluno)
+{
+    console.log(`Esta pessoa se chama ${nome}`);
+}
+
+
+let numeroDeAlunos = 10;
+let contador = 0;
+/*
+while(contador <= numeroDeAlunos )
+{
+
+        if(contador == 0)
+        {
+            console.log("O número atual é zero");
+        }
+
+
+        else if(contador % 2 != 0)
+        {
+            console.log (`O número ${contador} é impar`); //interpolação
+        }
+
+        else
+        {
+            console.log (`O número ${contador} é par`); 
+        }
+
+        contador++;
+
+}*/
+
+
+/*
+for (let contador = 0; contador <= numeroDeAlunos; contador++) 
+{
+        // if(contador == 0)
+        // {
+        //     console.log("O número" + contador + "é zero"); //concatenar 
+        // }
+
+        if(contador == 0)
+        {
+            console.log("O número atual é zero");
+        }
+
+
+        else if(contador % 2 == 0)
+        {
+            console.log (`O número ${contador} é par`); //interpolação
+        }
+
+        else
+        {
+            console.log (`O número ${contador} é impar`); 
+        }
+
+        
+}*/
